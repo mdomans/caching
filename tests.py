0cf1660ce7e3ac054cdbd468e5d0b056620dbae2
mdomans <michaldoman@k2.pl>
2014-12-27 20:42:43 +0100

    working params

--- tests.py
+++ tests.py
@@ -7,8 +7,8 @@ import unittest
 # from hash_ring import MemcacheRing
 from memcache import Client
 
-class Basic(unittest.TestCase):
 
+class Basic(unittest.TestCase):
     def setUp(self):
         mc = Client(['0.0.0.0:11211'])
         self.cache_decorator = cached_factory(mc)
@@ -36,6 +36,17 @@ class Basic(unittest.TestCase):
 
         self.assertEqual(func(1), 1)
         self.assertEqual(func(2), 2)
+
+        @cache("more_params")
+        def func2(argument, *args, **kwargs):
+            return argument, args, kwargs
+
+        self.assertEqual(func2(1, 2, test=3), (1, (2,), {"test": 3}))
+        self.assertEqual(func2(1, 45), (1, (45,), {}))
+        self.assertEqual(func2(1), (1, (), {}))
+
+    def test_groups(self):
+        pass
 
 
 
